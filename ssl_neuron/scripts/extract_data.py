1790df2adec62d9e0e01e1cbc61cd2dd0c8c6671
kpuhger <krpuhger@gmail.com>
2024-03-21 11:14:20 -0700

    add CLI version of data extraction code

---
+++ ssl_neuron/scripts/extract_data.py
@@ -0,0 +1,116 @@
+import pickle
+from pathlib import Path
+
+import networkx as nx
+import numpy as np
+from allensdk.core.swc import read_swc
+from tqdm import tqdm
+
+from ssl_neuron.data.data_utils import connect_graph, remove_axon
+from ssl_neuron.utils import neighbors_to_adjacency
+
+
+def read_morphology(swc_file: str | Path) -> tuple[dict, np.ndarray]:
+    """Reads the morphology data from an SWC file for a given cell ID."""
+    morphology = read_swc(swc_file)
+    return morphology
+
+
+def process_morphology(morphology: tuple[dict, np.ndarray]) -> tuple[dict, np.ndarray]:
+    """Processes the morphology to extract and normalize features."""
+    soma = morphology.soma
+    soma_pos = np.array([soma["x"], soma["y"], soma["z"]])
+    soma_id = soma["id"]
+    assert soma_id is not None
+    neighbors = {}
+    idx2node = {}
+    for i, item in enumerate(morphology.compartment_list):
+        sec_type = [0, 0, 0, 0]
+        sec_type[item["type"] - 1] = 1
+        feat = (item["x"], item["y"], item["z"], item["radius"], *sec_type)  # + tuple(sec_type)
+        idx2node[i] = feat
+        neighbors[i] = set(item["children"])
+        if item["parent"] != -1:
+            neighbors[i].add(item["parent"])
+
+    features = np.array(list(idx2node.values()))
+    assert not np.any(np.isnan(features))
+    norm_features = features.copy()
+    norm_features[:, :3] -= soma_pos
+    # Ensure the graph is connected
+    adj_matrix = neighbors_to_adjacency(neighbors, range(len(neighbors)))
+    G = nx.Graph(adj_matrix)
+    if nx.number_connected_components(G) > 1:
+        adj_matrix, neighbors = connect_graph(adj_matrix, neighbors, features)
+    neighbors, norm_features, _ = remove_axon(neighbors, norm_features, int(soma_id))
+    assert len(neighbors) == len(norm_features) and not np.any(np.isnan(norm_features))
+
+    return neighbors, norm_features
+
+
+def save_processed_data(path: Path, neighbors: dict, norm_features: np.ndarray) -> None:
+    """Saves the processed data to disk."""
+    np.save(Path(path, "features.npy"), norm_features)
+    with open(Path(path, "neighbors.pkl"), "wb") as f:
+        pickle.dump(neighbors, f, pickle.HIGHEST_PROTOCOL)
+
+
+def process_cell(swc_file: str | Path, output_dir: str | None = None) -> None:
+    """Processes a single cell, orchestrating the reading, processing, and saving of data.
+
+    Args:
+        swc_file (str): Path to the SWC file.
+    """
+    swc_file = Path(swc_file)
+    cell_id = str(swc_file.stem)
+    output_dir = output_dir or swc_file.parent
+    dpath = Path(f"{output_dir}/skeletons/{cell_id}")
+    dpath.mkdir(parents=True, exist_ok=True)
+    try:
+        morphology = read_morphology(swc_file)
+        neighbors, norm_features = process_morphology(morphology)
+        save_processed_data(dpath, neighbors, norm_features)
+    except Exception as e:
+        print(e)
+        print(f"Failed on {cell_id}")
+
+
+def process_all_cells(dpath: str, cell_ids: list[str], split: str = "all") -> None:
+    """Iterates over all cell IDs and processes them."""
+    processed_cell_ids = []
+    for cell_id in tqdm(cell_ids):
+        process_cell(dpath, cell_id)
+        processed_cell_ids.append(cell_id)
+
+    np.save(Path(dpath, f"{split}_ids.npy"), processed_cell_ids)
+
+
+if __name__ == "__main__":
+    from typer import Argument, Option, Typer
+
+    app = Typer()
+
+    @app.command()
+    def process_cells(
+        swc_path: str = Argument(..., help="Path to the SWC files."),
+        split: str = Option("all", "-s", "--split", help="Split to process.", show_default=True),
+        output_dir: str = Option(
+            None, "-o", "--output-dir", help="Output directory.", show_default=True
+        ),
+    ) -> None:
+        output_dir = output_dir or Path(swc_path).parent
+
+        processed = []
+        swc_files = list(Path(swc_path).glob("*.swc"))
+        for file in tqdm(swc_files, desc="Processing cells"):
+            try:
+                process_cell(file, output_dir)
+                processed.append(file.stem)
+            except Exception as e:
+                print(e)
+                print(f"Failed to process: {file.stem}")
+
+        print(f"Processed {len(processed)} cells. Saving IDs to {output_dir}/{split}_ids.npy")
+        np.save(f"{output_dir}/{split}_ids.npy", processed)
+
+    app()
